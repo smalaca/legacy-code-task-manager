1c014a120abc56c0262c93a97c388d3d1e84d1f9
smalaca <ab13.krakow@gmail.com>
2020-04-14 15:38:19 +0200

    FIX - CHECKSTYLE - add private constructor to TaskManagerApplication

--- src/main/java/com/smalaca/taskamanager/TaskManagerApplication.java
+++ src/main/java/com/smalaca/taskamanager/TaskManagerApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TaskManagerApplication {
+    private TaskManagerApplication() {}
+
     public static void main(String[] args) {
         SpringApplication.run(TaskManagerApplication.class, args);
     }
