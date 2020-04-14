baef902166d64a6d5a8edf93decb55538963ebac
smalaca <ab13.krakow@gmail.com>
2020-04-14 16:01:34 +0200

    remove private constructor from TaskManagerApplication

--- src/main/java/com/smalaca/taskamanager/TaskManagerApplication.java
+++ src/main/java/com/smalaca/taskamanager/TaskManagerApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @SuppressWarnings("checkstyle:HideUtilityClassConstructor")
 public class TaskManagerApplication {
-    private TaskManagerApplication() {}
-
     public static void main(String[] args) {
         SpringApplication.run(TaskManagerApplication.class, args);
     }
