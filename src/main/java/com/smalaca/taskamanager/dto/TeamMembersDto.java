0d129a43e8cf421326b9ab52f47182fdc097aad1
smalaca <ab13.krakow@gmail.com>
2020-07-08 16:19:40 +0200

    Remove unnecessary teamId attribute from TeamMembersDto

--- src/main/java/com/smalaca/taskamanager/dto/TeamMembersDto.java
+++ src/main/java/com/smalaca/taskamanager/dto/TeamMembersDto.java
@@ -3,16 +3,7 @@ package com.smalaca.taskamanager.dto;
 import java.util.List;
 
 public class TeamMembersDto {
-    private long teamId;
     private List<Long> userIds;
-
-    public void setTeamId(long teamId) {
-        this.teamId = teamId;
-    }
-
-    public long getTeamId() {
-        return teamId;
-    }
 
     public void setUserIds(List<Long> userIds) {
         this.userIds = userIds;
