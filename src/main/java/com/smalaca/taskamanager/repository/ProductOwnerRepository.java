e47160ca55c08accd784da0ccfe1fbd652f7f922
smalaca <ab13.krakow@gmail.com>
2020-10-27 21:19:07 +0100

    add method ProductOwnerRepository.findByFirstNameAndLastName

--- src/main/java/com/smalaca/taskamanager/repository/ProductOwnerRepository.java
+++ src/main/java/com/smalaca/taskamanager/repository/ProductOwnerRepository.java
@@ -4,6 +4,9 @@ import com.smalaca.taskamanager.domain.ProductOwner;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProductOwnerRepository extends CrudRepository<ProductOwner, Long> {
+    Optional<ProductOwner> findByFirstNameAndLastName(String firstName, String lastName);
 }
