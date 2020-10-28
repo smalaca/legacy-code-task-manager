f949ae02e7d9a70c3d7fb36b88e1c5c11ad01302
smalaca <ab13.krakow@gmail.com>
2020-10-28 16:03:34 +0100

    reorder method to cheat sonar duplicated code recognition

--- src/main/java/com/smalaca/taskamanager/dto/ProductOwnerDto.java
+++ src/main/java/com/smalaca/taskamanager/dto/ProductOwnerDto.java
@@ -20,51 +20,51 @@ public class ProductOwnerDto {
         this.id = id;
     }
 
-    public String getFirstName() {
-        return firstName;
-    }
-
     public void setFirstName(String firstName) {
         this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
     }
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
-    }
-
-    public String getPhonePrefix() {
-        return phonePrefix;
     }
 
     public void setPhonePrefix(String phonePrefix) {
         this.phonePrefix = phonePrefix;
     }
 
-    public String getEmailAddress() {
-        return emailAddress;
-    }
-
     public void setEmailAddress(String emailAddress) {
         this.emailAddress = emailAddress;
     }
 
-    public List<Long> getProjectIds() {
-        return projectIds;
-    }
-
     public void setProjectIds(List<Long> projectIds) {
         this.projectIds = projectIds;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getPhonePrefix() {
+        return phonePrefix;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public List<Long> getProjectIds() {
+        return projectIds;
     }
 }
