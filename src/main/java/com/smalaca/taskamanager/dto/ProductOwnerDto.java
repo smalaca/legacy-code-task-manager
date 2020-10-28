ed57b505b1ebe53ac52ca55636078d7f4fca373d
smalaca <ab13.krakow@gmail.com>
2020-10-28 14:48:11 +0100

    add ProductOwnerController

---
+++ src/main/java/com/smalaca/taskamanager/dto/ProductOwnerDto.java
@@ -0,0 +1,70 @@
+package com.smalaca.taskamanager.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductOwnerDto {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String phonePrefix;
+    private String emailAddress;
+    private List<Long> projectIds = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPhonePrefix() {
+        return phonePrefix;
+    }
+
+    public void setPhonePrefix(String phonePrefix) {
+        this.phonePrefix = phonePrefix;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public List<Long> getProjectIds() {
+        return projectIds;
+    }
+
+    public void setProjectIds(List<Long> projectIds) {
+        this.projectIds = projectIds;
+    }
+}
