dfd7bbe7e3efddd3a3b6f61ffe776114c6446b72
smalaca <ab13.krakow@gmail.com>
2020-11-06 11:43:45 +0100

    Introduce ToDoItemService and empty ToDoItemProcessor

---
+++ src/main/java/com/smalaca/taskamanager/processor/ToDoItemProcessor.java
@@ -0,0 +1,11 @@
+package com.smalaca.taskamanager.processor;
+
+import com.smalaca.taskamanager.model.interfaces.ToDoItem;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ToDoItemProcessor {
+    public void processFor(ToDoItem toDoItem) {
+
+    }
+}
